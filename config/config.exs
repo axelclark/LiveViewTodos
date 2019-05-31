a8c9629a7e14a902e730734910a4f074c2401e77
Axel Clark <axelclark2@yahoo.com>
2019-05-31 16:28:50 -0700

    Complete live view broadcast

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,10 @@ config :live_view_todos, LiveViewTodosWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "eCwRCjN9lWagwGGw9VgxA3H5cgXsOnSciajb8ldrbBFo2O3WKhBcML0Uw5uvLw+Q",
   render_errors: [view: LiveViewTodosWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: LiveViewTodos.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: LiveViewTodos.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [
+    signing_salt: "VbH1ZKM0EfZGfTjfwhRTJlPRBmTYytd5"
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -25,11 +28,6 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :my_app, LiveView.Endpoint,
-  live_view: [
-    signing_salt: "VbH1ZKM0EfZGfTjfwhRTJlPRBmTYytd5"
-  ]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config("#{Mix.env()}.exs")
